5a650aaa47de77ac9e780410d3a3f56571d98303
aryan9855 <aryansinghalagra@gmail.com>
2025-01-23 22:00:47 +0530

    basic learning of js

---
+++ dataTypes.js
@@ -0,0 +1,126 @@
+// name = "Aryan ";
+// console.log(name);
+// age = 20;
+// console.log(age);
+// x = null;
+// console.log(x); 
+// y = undefined;
+// console.log(y); 
+
+// isFollow = false;
+// console.log(isFollow);  
+
+// let agew =23;
+
+
+// let x = prompt("Enter the value of x");
+// if(x%5==0){
+//     console.log("Yes it is divisible by 5");
+// }
+// else{
+//     console.log("No it is not divisible by 5");
+// }
+
+
+//  let n = prompt("Enter the value of n");
+//  for(let i =0 ; i< n;i++){
+//      console.log("x");
+//  }
+
+// let str = "Aryan";
+// let lenght =0;
+// for(let i of str){
+//     lenght++;
+// }
+// console.log(lenght);
+
+// let hero = ["Aryan",88,"Rohit","Raj"];
+//  for(let i of hero){
+//      console.log(i);
+//  }
+
+// let marks = [85,97,44,37,76,60];
+// let sum=0 ;
+// for(let i of marks){
+//     sum += i;
+// }
+// let avg = sum/marks.length;
+// console.log(avg);
+
+
+// let prices = [250,645,300,900,50];
+// let i =0;
+// for(let price of prices){
+//     let offer = price /10;
+//     prices[i] = prices[i] - offer;
+//     console.log(prices[i]);
+//     i++;
+// }
+
+// let arr = [1,2,3,4,5];
+
+// for(let i =0 ; i<arr.length;i++){
+//         console.log(arr[i]);
+    
+// }
+
+// arr.splice(2,2,10,20);
+
+// for(let i =0 ; i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+
+// let arr = ["Bloomberg","Google","Microsoft","Uber","Apple","Amazon"];
+// arr.shift();
+// for(let i =0 ; i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// arr.splice(2,1,"Ola");
+
+// for(let i =0 ; i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// arr.push("Tesla");
+
+// for(let i =0 ; i<arr.length;i++){   
+//     console.log(arr[i]);
+// }
+
+
+//  function add(a,b){
+//     return a+b;
+//  }
+
+//  function mul(c,d){
+//         return c*d; 
+//  }
+
+
+// let mul = (a,b) =>{
+//     return a*b;
+// };
+
+//  function vowel(str){
+//     let count =0;
+//     for(let i of str){
+//         if(i == 'a'|| i == 'e'|| i== 'i' || i=='o'|| i=='u'){
+//             count++;
+//         }
+//     }
+//     return count;
+//  }
+
+
+//  let vowel = (str) => {
+//     let count =0;
+//     for(let i of str){
+//         if(i == 'a'|| i == 'e'|| i== 'i' || i=='o'|| i=='u'){
+//             count++;
+//         }
+//     }
+//     return count;
+//  }
+
